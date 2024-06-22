d958523346bf5ed74a58bd9aabb01f2434475197
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-06-23 04:56:15 +0800

    Element explorer.

--- src/display/app_description.c
+++ src/display/app_description.c
@@ -2,8 +2,9 @@
 #include "./color_codes.c"
 #include <stdio.h>
 
-// Include a function prototype for those functions already imported in main.
+// Function prototypes.
 void add_new_line(void);
+void add_new_tab(void);
 
 void app_description(void) {
   add_new_tab();
@@ -19,6 +20,6 @@ void app_description(void) {
   add_new_line();
   add_new_tab();
   add_new_tab();
-  printf("----------------------------------------------\n");
+  printf("%s----------------------------------------------%s\n", BLUE, RESET);
   add_new_line();
 }
--- src/main.c
+++ src/main.c
@@ -3,15 +3,22 @@
 #include "./display/clear_terminal.c"
 #include <stdio.h>
 
+// Function prototypes.
+void add_new_tab(void);
+void add_new_line(void);
+void clear_terminal(void);
+void add_description(void);
+
 int main(void) {
   // Clean up the screen.
   clear_terminal();
   add_new_line();
   add_new_line();
 
+  // Show the name of the tool and include some description.
   app_description();
 
-  // Add new line before exit.
+  // Add some spaces before the exit.
   add_new_line();
   add_new_line();
 
