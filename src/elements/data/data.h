928ca683dc2aa29d6c28f4710b0e45b5862fb835
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-08-01 06:56:09 +0800

    Remove the . and / in data.h

--- src/elements/data/data.h
+++ src/elements/data/data.h
@@ -1,12 +1,12 @@
 #ifndef DATA_H
 #define DATA_H
-#include "./elements_1st_period.c"
-#include "./elements_2nd_period.c"
-#include "./elements_3rd_period.c"
-#include "./elements_4th_period.c"
-#include "./elements_5th_period.c"
-#include "./elements_6th_period.c"
-#include "./elements_7th_period.c"
-#include "./elements_actinides.c"
-#include "./elements_lanthanides.c"
+#include "elements_1st_period.c"
+#include "elements_2nd_period.c"
+#include "elements_3rd_period.c"
+#include "elements_4th_period.c"
+#include "elements_5th_period.c"
+#include "elements_6th_period.c"
+#include "elements_7th_period.c"
+#include "elements_actinides.c"
+#include "elements_lanthanides.c"
 #endif
